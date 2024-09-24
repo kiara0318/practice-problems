0e7f960384a0ba54dc263daa46f4e4595d0fbcc7
Kiara Oliver <kiara.oliver318@gmail.com>
2024-09-24 02:57:08 -0400

    Add comments

--- Permutations.java
+++ Permutations.java
@@ -1,32 +1,51 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Permutations {
     
     public static void main(String[] args) {
+        // Initialize the list of elements to permute
         List<String> elements = Arrays.asList("a", "b", "c");
+        
+        // Generate all permutations of the elements
         List<String> permutations = generatePermutations(elements);
+        
+        // Print the generated permutations
         System.out.println(String.join(", ", permutations));
     }
 
+    // Function to generate all permutations of the given list of elements
     static List<String> generatePermutations(List<String> elements) {
+        // List to store all the permutations
         List<String> permutations = new ArrayList<String>();
+        
+        // Build the permutations starting with an empty current permutation
         buildPermutations(elements, "", permutations);
+        
+        // Return the list of generated permutations
         return permutations;
     }
 
+    // Recursive function to build permutations
     static void buildPermutations(List<String> availableElements, String currentPermutation, List<String> permutations) {
+        // Base case: if there are no more available elements, add the current permutation to the list
         if (availableElements.isEmpty()) {
-          permutations.add(currentPermutation);
-          return;
+            permutations.add(currentPermutation);
+            return;
         }
 
+        // Iterate over the available elements
         for (int i = 0; i < availableElements.size(); i++) {
-          String currentElement = availableElements.get(i);
-          List<String> remainingElements = new ArrayList<String>(availableElements);
-          remainingElements.remove(i);
-          buildPermutations(remainingElements, currentPermutation + currentElement, permutations);
+            // Get the current element to add to the permutation
+            String currentElement = availableElements.get(i);
+            
+            // Create a new list of remaining elements by excluding the current element
+            List<String> remainingElements = new ArrayList<String>(availableElements);
+            remainingElements.remove(i);
+            
+            // Recursively build permutations with the remaining elements and the updated current permutation
+            buildPermutations(remainingElements, currentPermutation + currentElement, permutations);
         }
     }
-}
-
+}  
